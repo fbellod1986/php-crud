8ddc8cbb8ea60cf53fc0148cc6017900d7820755
Franco Bello <franco.lauer@gmail.com>
2022-09-12 23:20:12 -0300

    primer commit

---
+++ Insertardatos.php
@@ -0,0 +1,37 @@
+<?php 
+  header('Access-Control-Allow-Origin: *'); 
+  header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+  
+  $json = file_get_contents('php://input'); // RECIBE EL JSON DE ANGULAR
+ 
+  $params = json_decode($json); // DECODIFICA EL JSON Y LO GUARADA EN LA VARIABLE
+  
+  require("../comun/db.php"); // IMPORTA EL ARCHIVO CON LA CONEXION A LA DB
+
+  $conexion = conexion(); // CREA LA CONEXION
+  
+  // REALIZA LA QUERY A LA DB
+  mysqli_query($conexion, "INSERT INTO personas(rut, plan, asunto, nombre, apellido, idTransaccion, miembroGitlab, email, mensaje) VALUES
+                  (
+                    '$params->rut', 
+                    '$params->plan', 
+                    '$params->asunto', 
+                    '$params->nombre', 
+                    '$params->apellido',
+                    '$params->idTransaccion',
+                    '$params->miembroGitlab',
+                    '$params->email',
+                    '$params->mensaje'
+                  )");    
+  
+  class Result {}
+
+  // GENERA LOS DATOS DE RESPUESTA
+  $response = new Result();
+  $response->resultado = 'OK';
+  $response->mensaje = 'SE REGISTRO EXITOSAMENTE EL USUARIO';
+
+  header('Content-Type: application/json');
+
+  echo json_encode($response); // MUESTRA EL JSON GENERADO
+?>
---
+++ modificarpersona.php
@@ -0,0 +1,36 @@
+<?php 
+  header('Access-Control-Allow-Origin: *'); 
+  header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+  
+  $json = file_get_contents('php://input');
+ 
+  $params = json_decode($json);
+  
+  require("../comun/db.php"); // IMPORTA EL ARCHIVO CON LA CONEXION A LA DB
+
+  $conexion = conexion(); // CREA LA CONEXION
+  
+  // REALIZA LA QUERY A LA DB
+  mysqli_query($conexion, "UPDATE personas SET rut='$params->rut',
+      plan='$params->plan',
+	    asunto='$params->asunto',
+	    nombre='$params->nombre',
+	    apellido='$params->apellido',
+	    idTransaccion='$params->idTransaccion',
+	    miembroGitlab='$params->miembroGitlab',
+	    email='$params->email',
+	    mensaje='$params->mensaje'
+    WHERE idPersona=$params->idPersona");
+    
+  
+  class Result {}
+
+  // GENERA LOS DATOS DE RESPUESTA
+  $response = new Result();
+  $response->resultado = 'OK';
+  $response->mensaje = 'EL USUARIO SE ACTUALIZO EXITOSAMENTE';
+
+  header('Content-Type: application/json');
+
+  echo json_encode($response); // MUESTRA EL JSON GENERADO
+?>
